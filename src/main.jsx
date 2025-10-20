122479ad2dee9e98302a8a01f080342c5c9eb73a
Omolade Lekan <omoladelekan99@gmail.com>
2025-10-20 07:38:07 +0100

    commit for today

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
+import StarRating from "./StarRating";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+
+    {/* useful for when we dont want to set a props for our star component, it defaults to 5 stars */}
+    <StarRating />
+  </StrictMode>
+);
