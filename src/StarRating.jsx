de561269a397bbf241d277863388dc964ba8b4cf
Omolade Lekan <omoladelekan99@gmail.com>
2025-10-21 06:13:39 +0100

    and another commit added

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyles = {
   display: "flex",
@@ -11,17 +12,31 @@ const starConteinerStyles = {
   fontWeight: "bold",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [hoverRating, setHoverRating] = useState(0);
+
+  const availableOption = hoverRating || rating || "";
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+    fontWeight: "600",
+  };
 
   function handleClick(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   function handleHover(rating) {
@@ -29,7 +44,7 @@ export default function StarRating({ maxRating = 5 }) {
   }
 
   return (
-    <div style={containerStyles}>
+    <div style={containerStyles} className={className}>
       <div style={starConteinerStyles}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -37,22 +52,47 @@ export default function StarRating({ maxRating = 5 }) {
             onClick={() => handleClick(i + 1)}
             filled={i + 1 <= (hoverRating || rating)}
             onHover={() => handleHover(i + 1)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{hoverRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[availableOption - 1]
+          : availableOption}
+      </p>
     </div>
   );
 }
 
-const starStyles = {
-  height: "24px",
-  width: "24px",
-  display: "block",
-  cursor: "pointer",
+// PropTypes validation
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  size: PropTypes.number,
+  color: PropTypes.string,
+  onSetRating: PropTypes.func,
+  messages: PropTypes.array,
+  className: PropTypes.string,
 };
 
-function Star({ onClick, filled, onHover }) {
+// PropTypes validation
+StarRating.defaultProps = {
+  maxRating: 5,
+  defaultRating: 0,
+  size: 24,
+  color: "#fbbf24",
+};
+
+function Star({ onClick, filled, onHover, color, size }) {
+  const starStyles = {
+    height: `${size}px`,
+    width: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
+
   return (
     <span
       role="button"
@@ -65,8 +105,8 @@ function Star({ onClick, filled, onHover }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -75,7 +115,7 @@ function Star({ onClick, filled, onHover }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
@@ -7,10 +7,36 @@ import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={24} color="blue" defaultRating={3} />
 
     {/* useful for when we dont want to set a props for our star component, it defaults to 5 stars */}
     <StarRating />
+
+    <TestRating />
   </StrictMode>
 );
+
+function TestRating() {
+  const [testRating, setTestRating] = useState(3);
+  const maxRating = 10;
+
+  return (
+    <div>
+      <StarRating
+        size={24}
+        onSetRating={setTestRating}
+        maxRating={maxRating}
+        defaultRating={testRating}
+      />
+      <p>
+        This movie was rated {testRating}/{maxRating}
+      </p>
+    </div>
+  );
+}
