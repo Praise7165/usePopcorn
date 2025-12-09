96795dedb2bdb592234d96f276728d989db8039d
Omolade Lekan <omoladelekan99@gmail.com>
2025-12-09 08:08:15 +0100

    added focus on enter button

--- src/assets/components/Search.jsx
+++ src/assets/components/Search.jsx
@@ -1,4 +1,25 @@
-export default function Search({ query, onSearch }) {
+import { useRef, useEffect } from "react";
+
+export default function Search({ query, onSearch, setQuery }) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      // check if any active element is equal to our input element
+
+      if (document.activeElement === inputRef.current) return;
+
+      if (e.code === "Enter") {
+        inputRef.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    // return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +27,7 @@ export default function Search({ query, onSearch }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => onSearch(e)}
+      ref={inputRef}
     />
   );
 }
